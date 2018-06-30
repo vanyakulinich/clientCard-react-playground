7b8c8d5c4ddabc8c5dba0d973fe9af3b2818eb91
vanyakulinich <kulinichvanya@gmail.com>
2018-06-30 23:32:54 +0300

    cors addded

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
+const cors = require('cors');
 const clients = require('./clients.json');
 const PORT = 3001;
+
+app.use(cors());
 
 app.get('/', function(req, res) {
     res.send(clients)
